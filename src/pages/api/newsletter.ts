323a88e47807ae6de37bf45e4d637fdaf07198ba
johnnyfinchdev <johnnyfinchdev@gmail.com>
2026-03-08 23:29:43 +0100

    Cambios en el from resend

--- src/pages/api/newsletter.ts
+++ src/pages/api/newsletter.ts
@@ -34,7 +34,7 @@ export const POST: APIRoute = async (context) => {
 		// 3. EL CAMBIO CLAVE: Esperar el envío y verificar el remitente
 		// Usamos una constante para capturar el resultado antes de retornar nada
 		const { data: mailData, error: mailError } = await resend.emails.send({
-			from: 'Newsletter - Hola Developers! <newsletter@holadevelopers.blog>',
+			from: 'newsletter@holadevelopers.blog',
 			to: [email.trim()],
 			subject: 'Nos alegra que te hayas unido a la Newsletter, Developer',
 			html: `        
